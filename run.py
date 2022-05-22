39d380602ba4275a33f8f333fdc1ecb368084d96
matcool <26722564+matcool@users.noreply.github.com>
2022-05-21 21:47:44 -0300

    what

--- run.py
+++ run.py
@@ -7,9 +7,9 @@ try:
     print("Geometry Dash 2.0+ to 1.9 Level Converter")
     print("by ~zmx")
     input_file = filedialog.askopenfilename(title='Open the level file', filetypes=(('GMD File', '*.gmd'),))
-    if not input_file: exit()
+    if not input_file: sys.exit()
     output_file = filedialog.asksaveasfilename(title='Output .gmd file', filetypes=(('GMD File', '*.gmd'),), defaultextension='gmd')
-    if not output_file: exit()
+    if not output_file: sys.exit()
 
     args = [input_file, '-o', output_file]
     
